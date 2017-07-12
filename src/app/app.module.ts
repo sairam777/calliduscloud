289f2884676f05b893b9c152aedf3b326dd6c69d
sairam allampall <sairamallampalli02@gmail.com>
2017-07-12 18:03:27 +0530

    secong commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PlanwizardComponent } from './planwizard/planwizard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PlanwizardComponent
+    PlanwizardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
